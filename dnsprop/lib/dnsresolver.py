7330723d43906f6f9fd66087eb3224d1fbaf4677
AlexRymdeko-Harvey <a.rymdekoharvey@gmail.com>
2016-06-22 19:13:48 -0400

    docstring

--- dnsprop/lib/dnsresolver.py
+++ dnsprop/lib/dnsresolver.py
@@ -5,27 +5,31 @@ import dns.resolver
 
 class dnsresolver(object):
 
+    VALID_RECORDS = ['A', 'MX', 'PTR', 'TXT', 'AAAA', 'CNAME', 'DHCHID', 'DNSKEY', 'IPSEC', 'KEY', 'NS', 'SIG', 'SRV', 'URI']
+
     def __init__(self, verbose=False):
         try:
             self.verbose = verbose
         except Exception as e:
             print e
 
-    def check_dns(self, ip, quer):
+    def check_dns_a(self, ip, query, record_type):
         """
         Checks a A Record for givin DNS server IP,
         using provided DNS name server.
 
         ip - name server for query
-        quer - the domain name for search
+        query - the domain name for search
         """
         ipaddr = []
+        dnsip = []
         ipaddr.append(ip)
         my_resolver = dns.resolver.Resolver()  # create a new instance named 'myResolver'
         my_resolver.nameservers = ipaddr
 
-        myAnswers = my_resolver.query(quer, 'A')  # Lookup the 'A' record(s) for google.com
+        myAnswers = my_resolver.query(query, record_type)  # Lookup the 'A' record(s) for google.com
         for a in myAnswers:
-            print a.address
+            dnsip.append(a.address)
+        return dnsip
 
 
